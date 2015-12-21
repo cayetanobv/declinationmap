6f5319feb23db468b8a62ae495847bc274882d2d
cayetanobv <cayetanobv@gmail.com>
2015-12-21 02:51:07 +0100

    refactorized code

--- npcompdec.py
+++ npcompdec.py
@@ -8,43 +8,38 @@ import numpy as np
 import geomag
 
 
+class DecMap(object):
 
-def __genLatLon(bbox, prec):
-    """
-    """
+    def __genLatLon(self, bbox, prec):
+        """
+        """
 
-    lat_max, lon_max, lat_min, lon_min = bbox
+        lat_max, lon_max, lat_min, lon_min = bbox
 
-    lat = np.linspace(lat_max, lat_min, (180. * prec) + 1)
-    lon = np.linspace(lon_min, lon_max, (360. * prec) + 1)
-    
-    return np.meshgrid(lat, lon, sparse=False, indexing='xy')
+        lat = np.linspace(lat_max, lat_min, (180. * prec) + 1)
+        lon = np.linspace(lon_min, lon_max, (360. * prec) + 1)
 
-
-def __magDec(lat, lon, wmmfl):
-    """
-    """
-    gm = geomag.geomag.GeoMag(wmmfl)
-    
-    mag = gm.GeoMag(lat,lon)
-    
-    return mag.dec
+        return np.meshgrid(lat, lon, sparse=False, indexing='xy')
 
 
-def run(bbox, prec, wmmfl="data/WMM.COF"):
-    """
-    """
-    coords = __genLatLon(bbox, prec)
-    
-    lat, lon = coords
-    
-    vectMagDec = np.vectorize(__magDec)
-    res = vectMagDec(lat, lon, wmmfl)
-    
-    #coords.append(res)
-    
-    return coords, res
+    def __magDec(self, lat, lon, wmmfl):
+        """
+        """
+        gm = geomag.geomag.GeoMag(wmmfl)
+
+        mag = gm.GeoMag(lat,lon)
+
+        return mag.dec
 
 
+    def build(self, bbox, prec, wmmfl="data/WMM.COF"):
+        """
+        """
+        coords = self.__genLatLon(bbox, prec)
 
+        lat, lon = coords
 
+        vectMagDec = np.vectorize(self.__magDec)
+        res = vectMagDec(lat, lon, wmmfl)
+
+        return(coords, res)
--- plotdec.py
+++ plotdec.py
@@ -1,28 +1,28 @@
 
 import numpy as np
-import npcompdec
+from npcompdec import DecMap
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 
 def plotWorldDec(coords, res, bbox):
-    
+
     fig=plt.figure(figsize=(20,12))
     ax = fig.add_axes([0.1,0.1,0.75,0.75])
-    
-    
+
+
     #m = Basemap(llcrnrlon=-180,llcrnrlat=-85,urcrnrlon=180,urcrnrlat=85,projection='mill')
     m = Basemap(llcrnrlon=bbox[3],llcrnrlat=bbox[2],urcrnrlon=bbox[1],urcrnrlat=bbox[0],projection='mill')
-    
+
     lat, lon = coords
     x,y = m(lon, lat)
-    
+
     ct_range = range(-200, 201, 2)
     cs = m.contour(x,y,res,ct_range,cmap=plt.cm.jet)
     #cs = m.contour(x,y,res,ct_range,cmap=plt.cm.coolwarm)
     #cs = m.contour(x,y,res,ct_range,colors="b")
     plt.clabel(cs, inline=1, fmt='%1i', fontsize=10)
-    
+
     m.drawcoastlines()
     m.drawstates()
     m.drawcountries()
@@ -33,15 +33,16 @@ def plotWorldDec(coords, res, bbox):
     plt.colorbar(cs, cax, format='%g') # draw colorbar
     plt.axes(ax)  # make the original axes current again
     plt.title("Declination World Map", fontsize=12)
-                
+
     plt.show()
-    
+
 def main():
     #(Maximum latitude, Maximum Longitude, lat_min, lon_min)
     bbox = (85., 180., -85., -180.)
     prec = 1
-    coords, res = npcompdec.run(bbox, prec)
-    
+    decmap = DecMap()
+    coords, res = decmap.build(bbox, prec)
+
     plotWorldDec(coords, res, bbox)
 
 if __name__ == '__main__':
